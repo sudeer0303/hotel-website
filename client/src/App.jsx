4c3280022bc4bdf8a74f52a0696fa7402aabe76e
Premnath-Raju <premnathritspaxios036@gmail.com>
2025-12-09 11:50:44 +0530

    fixed navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+// import React from 'react'
 import Navbar from './Components/Navbar.jsx'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  )
+
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("admin");
+  // const isOwnerPath = useLocation().pathname.includes("admin");
+  // console.log(isOwnerPath);
+
+    return (
+      <div>
+        { !isOwnerPath && <Navbar /> }
+        {/* <Navbar /> */}
+      </div>
+    )
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
 
-  // <BrowserRouter>
-  <>
-    <App />
-  </>
-
-  // </BrowserRouter>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  
 )
+
+
